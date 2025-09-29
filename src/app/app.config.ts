d1f9464bbc5bacf4cc031c321a9170fda34f58a3
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2025-09-28 22:04:43 -0300

    feat (auth): firebase authentication

---
+++ src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+// Imports do Firebase
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+
+    provideFirebaseApp(() => initializeApp(environment)),
+    provideAuth(() => getAuth()),
+  ],
+};
