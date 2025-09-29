8660b0e012860da6db4ab3341f8e3ba30a5cd4e2
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2025-09-28 21:53:35 -0300

    feat (auth): firebase authentication

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  apiKey: "AIzaSyC12ilyG-2VQlgvuxdlA-1xzMhzXlltUpY",
+  authDomain: "solid-project-74d6c.firebaseapp.com",
+  projectId: "solid-project-74d6c",
+  storageBucket: "solid-project-74d6c.firebasestorage.app",
+  messagingSenderId: "820460630071",
+  appId: "1:820460630071:web:e872eb8a6bed89407ecfb2"
+};
