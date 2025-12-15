e622a9e5539a1f623b07480ec15f5a53455e4cac
Fabricio <92959461+fabricio-araujo94@users.noreply.github.com>
2025-12-15 03:22:01 +0000

    fix(api): failed integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,16 +7,15 @@ import { routes } from './app.routes';
 // Imports do Firebase
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { environment } from '../environments/environment';
 import { API_URL } from './tokens/api.token';
-import { backend_api } from '../environments/backend-api';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    { provide: API_URL, useValue: backend_api.apiUrl },
+    { provide: API_URL, useValue: environment.backendApiUrl },
     provideRouter(routes),
     provideHttpClient(),
-    provideFirebaseApp(() => initializeApp(environment)),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
   ],
 };
